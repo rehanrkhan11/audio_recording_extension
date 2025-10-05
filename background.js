f035dfb35edd90424064f5f17f7712ea6fc3ebe5
rehanrkhan11 <rehanrazakhan91@gmail.com>
2025-10-05 19:17:37 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,4 @@
+// Background service worker stub. Kept minimal for Manifest V3.
+console.log('Meet Audio Recorder background worker loaded');
+
+// No background tasks required for this lightweight extension.
